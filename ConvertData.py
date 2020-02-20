d8fc3c7d8d8c7a28570246364c787536e239d024
WuYiyang <wuyiyang17@mails.tsinghua.edu.cn>
2020-02-20 14:58:51 +0000

    write hdf5 using PyTables and for loop

--- ConvertData.py
+++ ConvertData.py
@@ -30,5 +30,37 @@ nChannels = Len(np.array(ReadoutTree["ChannelId"].array()))
 EventID_2 = EventID.repeat(nChannels)
 Waveform = Waveform.reshape(nWave,WindowSize)
 
+
+class WaveformData(tables.IsDescription):
+    EventID    = tables.Int64Col(pos=0)
+    ChannelID  = tables.Int16Col(pos=1)
+    Waveform   = tables.Col.from_type('int16', shape=WindowSize, pos=2)
+
+class TriggerInfoData(tables.IsDescription):
+    EventID    = tables.Int64Col(pos=0)
+    Sec        = tables.Int32Col(pos=1)
+    NanoSec    = tables.Int32Col(pos=2)
+
+h5file = tables.open_file(sys.argv[2], mode="w", title="OneTonDetector",filters = tables.Filters(complevel=9))
+TriggerInfoTable = h5file.create_table("/", "TriggerInfo", TriggerInfoData, "Trigger info")
+triggerinfo = TriggerInfoTable.row
+WaveformTable = h5file.create_table("/", "Waveform", WaveformData, "Waveform")
+waveform = WaveformTable.row
+
+for i in range(nWave) :
+    waveform['EventID'] = EventID_2[i]
+    waveform['ChannelID'] = ChannelID[i]
+    waveform['Waveform'] = Waveform[i]
+    waveform.append()
+
+WaveformTable.flush()
+    
+for i in range(len(EventID)) :
+    triggerinfo['EventID'] = EventID[i]
+    triggerinfo['Sec'] = Sec[i]
+    triggerinfo['NanoSec'] = NanoSec[i]
+    triggerinfo.append()
+TriggerInfoTable.flush()
+h5file.close()
+
 print(time()-start)
-embed()
